45fa6cdc201a18e8d4ea1d373a7cfe3f83fb08da
Matheus Ferreira <matheus.silva@zup.com.br>
2019-07-17 07:25:11 -0300

    first commit

---
+++ src/main/kotlin/application/CamundaCourseApplication.kt
@@ -0,0 +1,11 @@
+package application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class CamundaCourseApplication
+
+fun main() {
+    runApplication<CamundaCourseApplication>()
+}
